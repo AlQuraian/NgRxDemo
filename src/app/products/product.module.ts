114368391417b9b6ef4e07b8766f2c41887e5e56
Mohammad AlQuraian <mquraian@imkan.com.sa>
2018-09-03 14:09:19 +0300

    Rename file

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
 
 import { SharedModule } from '../shared/shared.module';
 
 import { ProductShellComponent } from './product-shell/product-shell.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
-import { StoreModule } from '@ngrx/store';
-import { reducer } from './state/product.reduce';
+import { reducer } from './state/product.reducer';
 
 const productRoutes: Routes = [
   { path: '', component: ProductShellComponent }
